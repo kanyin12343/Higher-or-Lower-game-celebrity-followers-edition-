90c020ab53c87cee229f081325bfa89a75138e49
kanyin12343 <145735971+kanyin12343@users.noreply.github.com>
2023-09-22 07:06:29 -0500

    Update main.py

--- main.py
+++ main.py
@@ -8,31 +8,40 @@ from replit import clear
 score = 0
 game_over = False
 
+#Initialise with a random data input
 entry1 = random.choice(data)
 while not game_over:
+    # Gather details from the initial entry.
     name1 = entry1['name']
     description1 = entry1['description']
     country1 = entry1['country']
     followers1 = entry1['follower_count']
     
+    # Show information for option A
     print(f"Compare A: {name1}, a {description1}, from {country1}")
     from art import vs
     print(vs)
-    
+
+    # Choose a random alternative for option B, making sure it differs from option A.
     entry2 = random.choice(data)
     while entry2 == entry1:
         entry2 = random.choice(data)
-    
+        
+    # Information from the second item (option B) should be extracted.
     name2 = entry2['name']
     description2 = entry2['description']
     country2 = entry2['country']
     followers2 = entry2['follower_count']
-    
+
+    # Show information for option B
     print(f"Against B: {name2}, a {description2}, from {country2}")
     
+    # Ask users to predict which person has the most followers.
     guess = input("Who has more followers? Type 'A' or 'B': ")
     clear()
     print(logo)
+
+    # Verify if the user's assumption was accurate.
     if guess == 'A':
         if followers1 > followers2:
             score += 1
