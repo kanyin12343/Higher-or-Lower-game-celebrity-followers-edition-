0cf4ff37d52fb70e1a1899fb759648dde34edf42
kanyin12343 <145735971+kanyin12343@users.noreply.github.com>
2023-09-21 22:20:05 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,58 @@
+
+from art import logo
+print(logo)
+
+import random 
+from game_data import data
+from replit import clear
+score = 0
+game_over = False
+
+entry1 = random.choice(data)
+while not game_over:
+    name1 = entry1['name']
+    description1 = entry1['description']
+    country1 = entry1['country']
+    followers1 = entry1['follower_count']
+    
+    print(f"Compare A: {name1}, a {description1}, from {country1}")
+    from art import vs
+    print(vs)
+    
+    entry2 = random.choice(data)
+    while entry2 == entry1:
+        entry2 = random.choice(data)
+    
+    name2 = entry2['name']
+    description2 = entry2['description']
+    country2 = entry2['country']
+    followers2 = entry2['follower_count']
+    
+    print(f"Against B: {name2}, a {description2}, from {country2}")
+    
+    guess = input("Who has more followers? Type 'A' or 'B': ")
+    clear()
+    print(logo)
+    if guess == 'A':
+        if followers1 > followers2:
+            score += 1
+            print(f"You guessed it right! Your current score: {score}\n")
+        else:
+            game_over = True
+            print(f"Sorry, that's incorrect. Game over! Your current score: {score}\n")
+    elif guess == 'B':
+        if followers1< followers2:
+            score += 1
+            print(f"You guessed it right! Your current score: {score}\n ")
+        else:
+            game_over = True
+            print(f"Sorry, that's incorrect. Game over! Your current score: {score}\n")
+    else:
+        game_over = True
+        print(f"Invalid input. Game over! Your current score: {score}\n")
+    
+    entry1 = entry2  # Switch the positions of options A and B
+    
+   
+
+
